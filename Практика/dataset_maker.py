8547560ee2810d48f680a96967e2e617997ed1cb
Lera <tito_alba@mail.ru>
2018-02-08 16:01:25 +0300

    new

--- Практика/dataset_maker.py
+++ Практика/dataset_maker.py
@@ -1,9 +1,11 @@
 # Всего различных иероглифов: 250
 # Из них в тренировочной выборке: 192
 # В тестовой: 58
+# таблицы в базе данных: characters, query, train
 
 import sqlite3
 import random
+import numpy as np
 
 def dict_maker():
     d = {}  # ключ - иероглиф, значение - количество повторений
@@ -29,7 +31,7 @@ def dict_maker():
                 d[char] += 1
     return d
 
-def data_base():
+def characters_base():
     d = dict_maker()
     conn = sqlite3.connect('characters.db')
     c = conn.cursor()
@@ -47,7 +49,6 @@ def data_base():
 # normalized_sample - ставим числа в диапазон от 0.01 до 1.0 для формирования входных сигналов
 
     rand_sample = [random.uniform(0, 1) for i in range(len(d))]
-    print(rand_sample)
     for i, key in enumerate(d):
         c.execute('''
         INSERT INTO characters (character, frequency, sample, normalized_sample) 
@@ -59,5 +60,79 @@ def data_base():
                 ''')
 
     result = c.fetchall()
-
+    conn.commit()
     conn.close()
+
+
+# векторизация тестовой выборки
+# cоздание новой таблицы - фраза|векторизованный вариант
+# векторизованная строка - ТЕКСТ!
+
+def query_base():
+    conn = sqlite3.connect('characters.db')
+    c = conn.cursor()
+    with open('query.txt', 'r', encoding='UTF-8') as q:
+        query = q.read().split()
+    q_vectorized = []
+    for row in query:
+        line = []
+        for char in row:
+            c.execute('''SELECT normalized_sample
+                    FROM characters 
+                    WHERE character == ?
+                    ''', [char])
+            result = c.fetchall()
+            line.append(result[0][0])
+        q_vectorized.append(line)
+
+    c.executescript("""DROP TABLE IF EXISTS query;
+
+                 CREATE TABLE query
+                 (phrase TEXT, 
+                 vectorization);
+                       """)
+    for i in range(len(query)):
+        c.execute('''INSERT INTO query (phrase, vectorization)
+                    VALUES (?, ?)''',
+                  [query[i], ' '.join(map(str, q_vectorized[i]))])
+    conn.commit()
+    conn.close()
+
+# тренировочная база
+# target придется делать вручную
+
+def train_base():
+    conn = sqlite3.connect('characters.db')
+    c = conn.cursor()
+    with open('train.txt', 'r', encoding='UTF-8') as t:
+        train = t.read().split()
+    train[0] = train[0].replace('\ufeff', '')
+    t_vectorized = []
+    for row in train:
+        line = []
+        for char in row:
+            c.execute('''SELECT normalized_sample
+                        FROM characters 
+                        WHERE character == ?
+                        ''', [char])
+            result = c.fetchall()
+            line.append(result[0][0])
+        t_vectorized.append(line)
+
+    c.executescript("""DROP TABLE IF EXISTS train;
+
+                 CREATE TABLE train
+                 (phrase TEXT,
+                 vectorization,
+                 target TEXT, 
+                 arr_target);
+                       """)
+    for i in range(len(train)):
+        c.execute('''INSERT INTO train (phrase, vectorization, target)
+                    VALUES (?, ?, ?)''',
+                  [train[i], ' '.join(map(str, t_vectorized[i])), train[i]])
+    conn.commit()
+    conn.close()
+
+
+train_base()
